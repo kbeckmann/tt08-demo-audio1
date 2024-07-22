7ffac43b9d89bb8998124808eabdc2bc0874af57
Konrad Beckmann <konrad.beckmann@gmail.com>
2024-07-23 00:49:59 +0200

    WIP, test

---
+++ src/tt_um_vga_example.v
@@ -0,0 +1,80 @@
+/*
+ * Copyright (c) 2024 Konrad Beckmann
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+`default_nettype none
+
+module tt_um_vga_example(
+  input  wire [7:0] ui_in,    // Dedicated inputs
+  output wire [7:0] uo_out,   // Dedicated outputs
+  input  wire [7:0] uio_in,   // IOs: Input path
+  output wire [7:0] uio_out,  // IOs: Output path
+  output wire [7:0] uio_oe,   // IOs: Enable path (active high: 0=input, 1=output)
+  input  wire       ena,      // always 1 when the design is powered, so you can ignore it
+  input  wire       clk,      // clock
+  input  wire       rst_n     // reset_n - low to reset
+);
+
+  // VGA signals
+  wire hsync;
+  wire vsync;
+  wire [1:0] R;
+  wire [1:0] G;
+  wire [1:0] B;
+  wire video_active;
+  wire [9:0] pix_x;
+  wire [9:0] pix_y;
+
+  // Audio signals
+  wire audio_out;
+  wire [15:0] audio_sample;
+
+  // TinyVGA PMOD
+  assign uo_out = {hsync, B[0], G[0], R[0], vsync, B[1], G[1], R[1]};
+
+  // Audio PMOD
+  assign uio_out = {audio_out, 7'b0000000};
+  assign uio_oe = 8'b10000000;
+
+  // Suppress unused signals warning
+  wire _unused_ok = &{ena, ui_in, uio_in};
+
+  reg [9:0] counter;
+
+// Audio start
+  pdm pdm_gen(
+    .clk(clk),
+    .rst_n(rst_n),
+    .pdm_in(audio_sample),
+    .pdm_out(audio_out)
+  );
+
+  wire [15:0] triangle = counter[16] ? -counter : counter;
+  assign audio_sample = triangle;
+// Audio end
+
+
+  hvsync_generator hvsync_gen(
+    .clk(clk),
+    .reset(~rst_n),
+    .hsync(hsync),
+    .vsync(vsync),
+    .display_on(video_active),
+    .hpos(pix_x),
+    .vpos(pix_y)
+  );
+  
+  assign R = video_active ? {counter[8], counter[9]} : 2'b00;
+  assign G = video_active ? {counter[4], counter[5]} : 2'b00;
+  assign B = video_active ? {counter[6], counter[7]} : 2'b00;
+  
+  always @(posedge vsync) begin
+    if (~rst_n) begin
+      counter <= 0;
+    end else begin
+      counter <= counter + 1;
+    end
+  end
+  
+endmodule
