c92909ba43e9a160405fecdf8a2cdd753e4cc808
Konrad Beckmann <konrad.beckmann@gmail.com>
2024-07-30 14:13:37 +0200

    case_256x8

--- src/gen.py
+++ src/gen.py
@@ -2,8 +2,8 @@ import random
 
 with open("rom.v", "w") as rom:
     for i in range(256):
-        # rom.write(f"      8'h{i:03X}: rom_data = 8'h{random.randint(0,255):02X};\n")
-        rom.write(f"  rom_content[{i}] = 8'h{random.randint(0,255):02X};\n")
+        rom.write(f"      8'h{i:02X}: rom_data = 8'h{random.randint(0,255):02X};\n")
+        # rom.write(f"  rom_content[{i}] = 8'h{random.randint(0,255):02X};\n")
         # rom.write(f"  rom_content_h[{i}] = 4'h{random.randint(0,15):01X}; ")
         # rom.write(f"  rom_content_l[{i}] = 4'h{random.randint(0,15):01X};\n")
     rom.close()
