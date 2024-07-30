1e388b3116c224f3b4704526b8195209cd5850a1
Konrad Beckmann <konrad.beckmann@gmail.com>
2024-07-30 13:00:04 +0200

    checkpoint, this works

---
+++ src/gen.py
@@ -0,0 +1,9 @@
+import random
+
+with open("rom.v", "w") as rom:
+    for i in range(128):
+        # rom.write(f"      8'h{i:03X}: rom_data = 8'h{random.randint(0,255):02X};\n")
+        # rom.write(f"  rom_content[{i}] = 8'h{random.randint(0,255):02X};\n")
+        rom.write(f"  rom_content_h[{i}] = 4'h{random.randint(0,15):01X}; ")
+        rom.write(f"  rom_content_l[{i}] = 4'h{random.randint(0,15):01X};\n")
+    rom.close()
