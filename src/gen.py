0fe99ec7bc37dc95aafcf3e2f2e6559a6bb7fd31
Konrad Beckmann <konrad.beckmann@gmail.com>
2024-07-31 20:34:39 +0200

    array_1024x8

--- src/gen.py
+++ src/gen.py
@@ -1,7 +1,7 @@
 import random
 
 with open("rom.v", "w") as rom:
-    for i in range(512):
+    for i in range(1024):
         # rom.write(f"      8'h{i:03X}: rom_data = 8'h{random.randint(0,255):02X};\n")
         rom.write(f"  rom_content[{i}] = 8'h{random.randint(0,255):02X};\n")
         # rom.write(f"  rom_content_h[{i}] = 4'h{random.randint(0,15):01X}; ")
